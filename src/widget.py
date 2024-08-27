fdd61512aeeaf32364a3aa890331927761b3e390
Nikita <nikkitos.pershin.2000@mail.ru>
2024-08-27 19:42:58 +0500

    init

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,7 @@
-from masks import get_mask_card_number,get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
-def mask_account_card (card: str) -> str:
+
+def mask_account_card(card: str) -> str:
     """Возвращает строку с замаскированным номером."""
     list_numbers = []
     for i in card:
@@ -13,10 +14,10 @@ def mask_account_card (card: str) -> str:
     else:
         return get_mask_account(card)
 
+
 def get_date(date: str) -> str:
     """Возвращает строку с датой в формате 'ДД.ММ.ГГГГ'."""
     return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
-
 
 
 user_date = "2024-03-11T02:26:18.671407"
