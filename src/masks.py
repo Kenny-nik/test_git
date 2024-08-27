7499514260c075188e621480d68dae8f07c683f6
Nikita <nikkitos.pershin.2000@mail.ru>
2024-08-27 19:39:26 +0500

    коммит инициализации

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,36 @@
 def get_mask_card_number(card: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
-    return f"{card[0:4]} {card[4:6]}** **** {card[12:16]}"
+    card_split = card.split()
+    list_name_card = []
+    list_numbers = []
+
+    for i in card_split:
+        if i.isalpha():
+            list_name_card.append(i)
+
+    for i in card:
+        if i.isdigit():
+            list_numbers.append(i)
+
+    name_card = " ".join(list_name_card)
+    numbers_card = "".join(list_numbers)
+    return f"{name_card} {numbers_card[0:4]} {numbers_card[4:6]}** **** {numbers_card[12:16]}"
 
 
 def get_mask_account(acc: str) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
-    return f"** {acc[12:16]}"
+    card_split = acc.split()
+    list_name_card = []
+    list_numbers = []
 
+    for i in card_split:
+        if i.isalpha():
+            list_name_card.append(i)
 
-card_number = input()
-account = input()
+    for i in acc:
+        if i.isdigit():
+            list_numbers.append(i)
 
-print(get_mask_card_number(card_number))
-print(get_mask_account(account))
+    name_card = " ".join(list_name_card)
+    numbers_card = "".join(list_numbers)
+    return f"{name_card} **{numbers_card[12:16]}"
---
+++ src/widget.py
@@ -0,0 +1,26 @@
+from masks import get_mask_card_number,get_mask_account
+
+def mask_account_card (card: str) -> str:
+    """Возвращает строку с замаскированным номером."""
+    list_numbers = []
+    for i in card:
+        if i.isdigit():
+            list_numbers.append(i)
+    if len(list_numbers) == 16:
+        return get_mask_card_number(card)
+    elif len(list_numbers) == 0:
+        return "Вы не ввели номер карты или счета!"
+    else:
+        return get_mask_account(card)
+
+def get_date(date: str) -> str:
+    """Возвращает строку с датой в формате 'ДД.ММ.ГГГГ'."""
+    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+
+
+
+user_date = "2024-03-11T02:26:18.671407"
+print(get_date(user_date))
+
+user_input = input()
+print(mask_account_card(user_input))
