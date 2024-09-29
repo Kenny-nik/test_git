91b91f7de655a72028381ad6ae3487bd7481dc30
Nikita <nikkitos.pershin.2000@mail.ru>
2024-09-29 21:23:47 +0500

    add logging masks.py and utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,19 @@
 from typing import Union
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('..\\logs\\masks.log', mode="w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 def get_mask_card_number(card: Union[str, int]) -> Union[str, int]:
     """Принимает на вход номер карты и возвращает ее маску."""
     card_split = str(card)
     list_numbers = [i for i in card_split if i.isdigit()]
     if len(list_numbers) == 16:
+        logger.warning(f'Некорректный номер карты: длинна карты = {len(list_numbers)}')
         list_letters = [i for i in card_split]
         list_1 = [
             (
@@ -21,6 +29,7 @@ def get_mask_card_number(card: Union[str, int]) -> Union[str, int]:
         result = "".join(list_1)
         return result
     else:
+        logger.info(f'Некорректный ввод номера карты')
         return "Некорректный ввод"
 
 
@@ -29,6 +38,7 @@ def get_mask_account(acc: Union[str, int]) -> Union[str, int]:
     acc_split = str(acc)
     list_numbers = [i for i in acc_split if i.isdigit()]
     if len(list_numbers) == 20:
+        logger.warning(f'Некорректный номер карты: длинна карты = {len(list_numbers)}')
         list_letters = [i for i in acc_split]
         list_1 = [
             (
@@ -44,4 +54,5 @@ def get_mask_account(acc: Union[str, int]) -> Union[str, int]:
         return result
 
     else:
+        logger.info(f'Некорректный ввод номера счета')
         return "Некорректный ввод"
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,16 @@
 import json
 import os
+import logging
 from typing import Any, List
 
 from src.external_api import currency_conversion_in_rub
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('..\\logs\\utils.log', mode="w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
 
@@ -13,16 +21,21 @@ def get_data_from_json(path: str) -> List[dict] | Any:
     возвращает список словарей с данными о финансовых транзакциях.
     """
     try:
+        logger.info('Открытие файла operations.json')
         with open(path, encoding="utf-8") as data_file:
             try:
+                logger.info('Преобразование транзакций из JSON-файла в список словарей')
                 transactions = json.load(data_file)
                 if isinstance(transactions, list):
                     return transactions
                 else:
+                    logger.info('Пустой файл')
                     return []
-            except json.JSONDecodeError:
+            except json.JSONDecodeError as ex:
+                logger.error(f'Ошибка {ex}')
                 return []
     except FileNotFoundError:
+        logger.error('Ошибка FileNotFoundError')
         return []
 
 
@@ -33,9 +46,13 @@ def get_amount_in_rub(transaction: dict) -> float | str:
     """
 
     try:
+        logger.info('Открытие файла operations.json')
         if transaction["operationAmount"]["currency"]["code"] == "RUB":
+            logger.info('Возврат суммы транзакции')
             return float(transaction["operationAmount"]["amount"])
         else:
+            logger.info('вызов функции конвертации валют')
             return currency_conversion_in_rub(transaction)
     except KeyError:
+        logger.error('Ошибка KeyError')
         return "Транзакция не найдена"
